d1a448b2ed6ed0866e8fd52ebee9309762e6f0e2
Shuvam Pal <ishuvam.pal@gmail.com>
2025-10-14 20:20:01 +0530

    fix problem-dto

--- internal/models/dto/contest-dto.go
+++ internal/models/dto/contest-dto.go
@@ -18,27 +18,3 @@ type GetContestProblemsResponse struct {
 	ContestID string            `json:"contest_id"`
 	Problems  []ProblemOverview `json:"problems"`
 }
-
-type ProblemOverview struct {
-	ID     string `json:"id"`
-	Name   string `json:"name"`
-	Score  int    `json:"score"`
-	Type   string `json:"type"`   // "code" or "mcq"
-	Status string `json:"status"` // "solved", "attempted", "not_attempted"
-}
-
-// GetProblemStatementResponse represents the response for getting problem statement
-type GetProblemStatementResponse struct {
-	ProblemID   string `json:"problem_id"`
-	ContestID   string `json:"contest_id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Score       int    `json:"score"`
-	Type        string `json:"type"` // "code" or "mcq"
-	// // For code problems
-	// TimeLimit   int `json:"time_limit"`   // milliseconds
-	// MemoryLimit int `json:"memory_limit"` // MB
-	// // For MCQ problems
-	// Options  []string `json:"options"`  // Available options
-	// Multiple bool     `json:"multiple"` // Single or multiple choice
-}
