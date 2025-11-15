fe71e72a95a2d2fad4a8491272ea49a01fbb1872
Soumya Pattanayak <smpsmp13@gmail.com>
2025-11-15 08:21:22 +0000

    fix: Add missing localhost CORS origin for development

--- internal/server.go
+++ internal/server.go
@@ -22,6 +22,7 @@ func NewEchoServer(
 			"https://recruitment.pointblank.club",
 			"http://recruitment.pointblank.club",
 			"http://localhost:5173",
+			"http://127.0.0.1:5173",
 		},
 		AllowMethods: []string{
 			echo.GET,
